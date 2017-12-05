2cf44a53e402f1b5e780b0083f823f06eb015ee4
Jason Simmons <celetavaro@gmail.com>
2017-12-05 15:47:27 -0700

    added garbage collection

--- plot1.R
+++ plot1.R
@@ -21,6 +21,9 @@ pdata <- read.table("household_power_consumption.txt", sep = ";", na.strings = "
 # subset for the dates we want to use ("2007-02-01", {2007-02-02})    
     testSet <- subset(pdata, pdata$Date == "2007-02-01" | pdata$Date == "2007-02-02")
     
+    # remove original dataset to free memory
+      rm(pdata)
+    
 # generate the Histogram
     par(mar = c(5.1, 4.1, 4.5, 2.1)) # extend top margin so export won't cut off upper limit of y-axis 
     
