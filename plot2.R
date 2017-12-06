a87ebf4bbd28222de4e7ca7e1937108d7fe4a869
Jason Simmons <celetavaro@gmail.com>
2017-12-06 13:12:55 -0700

    added to .gitignore, added plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,18 @@
+pdata <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
+
+pdata[, 1] <- as.Date(pdata[, 1], format = "%d/%m/%Y")
+
+# subset for the dates we want to use ("2007-02-01", "2007-02-02")    
+testSet <- subset(pdata, pdata$Date == "2007-02-01" | pdata$Date == "2007-02-02")
+
+testSet <- na.omit(testSet)
+
+# remove original dataset to free memory
+rm(pdata)
+
+fixTime <- strptime(paste(testSet$Date, testSet$Time, sep = " "), "%Y-%m-%d %H:%M:%S")
+gap <- as.numeric(testSet$Global_active_power)
+
+png("plot2.png", width = 480, height = 480)
+plot(fixTime, gap, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+dev.off()
