7a08530a3ae92c296ac0e2ade8b08f4fd5f9f6d8
Jason Simmons <celetavaro@gmail.com>
2017-12-06 14:00:34 -0700

    minor tweaks to R script, generated new plot

--- plot3.R
+++ plot3.R
@@ -12,10 +12,11 @@ rm(pdata)
 
 fixTime <- strptime(paste(testSet$Date, testSet$Time, sep = " "), "%Y-%m-%d %H:%M:%S")
   
+  # Open file device, plot, and close device
   png("plot3.png", width = 480, height = 480)
-  plot(fixTime, testSet$Sub_metering_1, xlab = "", ylab = "Energy Sub Metering", type = "l")
-  lines(fixTime, testSet$Sub_metering_2, type = "l", col = "red")
-  lines(fixTime, testSet$Sub_metering_3, type = "l", col = "blue")
-  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, 
-         col = c("black", "red", "blue"), cex = 0.70)
+    plot(fixTime, testSet$Sub_metering_1, xlab = "", ylab = "Energy Sub Metering", type = "l")
+    lines(fixTime, testSet$Sub_metering_2, type = "l", col = "red")
+    lines(fixTime, testSet$Sub_metering_3, type = "l", col = "blue")
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1,  
+           col = c("black", "red", "blue"), cex = 0.75)
   dev.off()
