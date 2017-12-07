7ce81cb424d374a786bc432c5c6a180e57ef0990
Jason Simmons <celetavaro@gmail.com>
2017-12-06 20:05:51 -0700

    final update to plot4.R, .gitignore, and final generated plot4.png

--- plot4.R
+++ plot4.R
@@ -11,14 +11,22 @@ testSet <- na.omit(testSet)
 rm(pdata)
 
 fixTime <- strptime(paste(testSet$Date, testSet$Time, sep = " "), "%Y-%m-%d %H:%M:%S")
+gap <- as.numeric(testSet$Global_active_power)
 
-par(mfcol = c(2,2), mar = c(4,4,4,0))
 
-plot(fixTime, gap, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
 
-plot(fixTime, testSet$Sub_metering_1, xlab = "", ylab = "Energy Sub Metering", type = "l")
-lines(fixTime, testSet$Sub_metering_2, type = "l", col = "red")
-lines(fixTime, testSet$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1,  
-       col = c("black", "red", "blue"), cex = 0.35)
-
+png("plot4.png", width = 480, height = 480)
+par(mfcol = c(2,2), mar = c(5,4,5,2))
+    # panel plot 1
+    plot(fixTime, gap, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+    # pane plot 2
+    plot(fixTime, testSet$Sub_metering_1, xlab = "", ylab = "Energy Sub Metering", type = "l")
+    lines(fixTime, testSet$Sub_metering_2, type = "l", col = "red")
+    lines(fixTime, testSet$Sub_metering_3, type = "l", col = "blue")
+    legend("topright", legend =  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1,  
+           col = c("black", "red", "blue"), bty = "n")
+    # pane plot 3
+    plot(fixTime, testSet$Voltage, xlab = "datetime", ylab = "Voltage", type = "l")
+    # pane plot 4
+    plot(fixTime, testSet$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", type = "l")
+  dev.off()
